eba7e847686f2f1c9e7167c05854bd11375ba529
HasmikQa <hasmik.sahakyan.a@gmail.com>
2021-02-07 11:50:10 +0400

    add comment

--- src/main/java/init/InitDriverManualy.java
+++ src/main/java/init/InitDriverManualy.java
@@ -3,15 +3,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class InitDriverManualy {
-    public static void main(String[]args){
+    public static void main(String[]args) throws InterruptedException {
         //todo version problem show
         System.setProperty("webdriver.chrome.driver","C:/Users/Gayane/Downloads/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.google.com/");
-
-
-        driver.findElement(By.id("menu")).click();
+        driver.get("https://staff.am/");
+         Thread.sleep(5000);
+         driver.findElement(By.className("hs_nav_link")).click();
+         driver.findElement(By.name("JobsFilter[key_word]")).sendKeys("C#");
+         driver.findElement(By.id("btn_search_keyword")).click();
+         driver.findElement(By.className("btn btn-primary mrgr0 radius_changes")).click();
+         driver.findElement((By.cssSelector()))
     }
 
 
